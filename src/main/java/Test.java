5732ead3879c630ac0a3dbce2608098baf3cf02e
allNormal <61479925+allNormal@users.noreply.github.com>
2020-10-06 01:32:58 +0200

    excel poi is as much as done, added macroreader also, need ontologyconnector next

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,13 +3,6 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-       /* dataAnalyse file1 = new dataAnalyse("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\reptile_checklist_2020_08.xlsx");
-        dataAnalyse file2 = new dataAnalyse("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\Endangered_Species.xlsx");
-
-        System.out.println(file1.checkRawDataTypes());
-
-        System.out.println(file2.checkColumnDataTypes("Species"));
-        file1.checkAllColumnDataTypes();
-        */
+       readExcel file1 = new readExcel("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\simple_test.xlsm");
     }
 }
--- src/main/java/readExcel.java
+++ src/main/java/readExcel.java
@@ -6,6 +6,9 @@ import java.util.*;
 
 import excel.Workbook.Workbook;
 import excel.Worksheet.Worksheet;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.poifs.macros.Module;
+import org.apache.poi.poifs.macros.VBAMacroReader;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.*;
@@ -48,13 +51,31 @@ public class readExcel {
             readTable(myWorkBook, sheet.getSheetName());
             readChart(myWorkBook, sheet.getSheetName());
             readIllustration(myWorkBook, sheet.getSheetName());
+            readText(myWorkBook, sheet.getSheetName());
             readBasicElement(sheet);
         }
+        readMacro(myFile);
 
+    }
+
+    private void readMacro(File file) {
+        try {
+            VBAMacroReader macroReader = new VBAMacroReader(file);
+            if(macroReader != null) {
+                Map<String, String> macro = macroReader.readMacros();
+                macroReader.close();
+                for(Map.Entry<String, String> m : macro.entrySet()){
+                    System.out.println(m.getValue());
+                }
+            }
+        } catch (IOException err) {
+            System.out.println(err);
+        }
 
     }
 
     private void readText(XSSFWorkbook workbook, String sheetName) {
+        int count = 0;
         try {
             XSSFSheet sheet = workbook.getSheet(sheetName);
             if(sheet == null) throw new FileNotFoundException("there is not sheet with name " + sheetName);
@@ -65,10 +86,11 @@ public class readExcel {
                 List<XSSFShape> shapes = shape.getShapes();
 
                 for(int i = 0;i<shapes.size();i++){
-                    if(shapes.get(i) instanceof  XSSFTextBox){
-
+                    if(shapes.get(i) instanceof  XSSFSimpleShape){
+                        count++;
                     }
                 }
+                System.out.println("there are " + count + " of texts");
             }
         } catch (FileNotFoundException err) {
             System.out.println(err);
@@ -76,6 +98,7 @@ public class readExcel {
     }
 
     private void readIllustration(XSSFWorkbook workbook, String sheetName){
+        int count = 0;
         try {
             XSSFSheet sheet = workbook.getSheet(sheetName);
             if(sheet == null) throw new FileNotFoundException("there is not sheet with name " + sheetName);
@@ -86,13 +109,14 @@ public class readExcel {
                 List<XSSFShape> charts = chart.getShapes();
                 for(int i = 0;i<charts.size();i++){
                     if(charts.get(i) instanceof Picture){
-
+                        count++;
                     }
                 }
             }
         } catch (FileNotFoundException err) {
             System.out.println(err);
         }
+        System.out.println("there are " + count + " of pictures");
     }
 
     private void readChart(XSSFWorkbook workbook, String sheetName){
@@ -104,11 +128,11 @@ public class readExcel {
             if(chart != null) {
                 //add to ontology
                 List<XSSFChart> charts = chart.getCharts();
+                System.out.println("there are " + charts.size() + " of charts");
             }
         } catch (FileNotFoundException err) {
             System.out.println(err);
         }
-
     }
 
     private void readBasicElement(Sheet sheet) {
@@ -139,7 +163,7 @@ public class readExcel {
             if(sheet == null) throw new FileNotFoundException("there is not sheet with name " + sheetName);
             List<XSSFTable> tables = sheet.getTables();
             List<XSSFPivotTable> pivotTables = sheet.getPivotTables();
-
+            System.out.println("there are " + tables.size() + " of tables");
             while(tables.size() != 0 && pivotTables.size()!= 0){
 
                 tables.get(0).getEndColIndex();
