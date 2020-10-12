9c0d5c34b7ed9464d7ad22f3742d48a39f7e629b
allNormal <61479925+allNormal@users.noreply.github.com>
2020-10-12 11:58:46 +0200

    a little bug fixes (blank nodes in cell-value fixed)

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,8 +4,8 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         OntologyService o = new OntologyService("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\simple_test.xlsm");
-        OntologyService o1 = new OntologyService("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\OBARIS SC2 Alle_Parameter_2007-2013_Basisversion_Erosion(1).xlsm");
-        o1.saveModel("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\OBARIS SC2 Alle_Parameter_2007-2013_Basisversion_Erosion(1).xlsm");
+        //OntologyService o1 = new OntologyService("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\OBARIS SC2 Alle_Parameter_2007-2013_Basisversion_Erosion(1).xlsm");
+        //o1.saveModel("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\OBARIS SC2 Alle_Parameter_2007-2013_Basisversion_Erosion(1).xlsm");
         o.saveModel("C:\\Users\\43676\\Desktop\\uni\\sepm-individual-assignment-java-template\\template\\rdfTest\\src\\main\\resources\\simple_test.xlsm");
     }
 }
